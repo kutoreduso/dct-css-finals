11fde9799172d392661c3bbe2eb7f484c338e1a2
kutoreduso <kurtcantiga16@gmail.com>
2024-11-21 00:46:40 +0800

    database successfully connected!

--- functions.php
+++ functions.php
@@ -1,4 +1,12 @@
 <?php    
     // All project functions should be placed here
-
+    function dbConnect() {
+        $host = "localhost"; // Server hostname
+        $username = "root";  // Database username
+        $password = "";      // Database password
+        $database = "dct-ccs-finals"; // Database name
+    
+        // Create a new connection
+        $conn = new mysqli($host, $username, $password, $database);
+    }
 ?>
